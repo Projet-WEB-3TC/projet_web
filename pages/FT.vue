ff0d628e98e394b28bdd771dc49da6fc140925e5
Clément Baroudi <clement.baroudi@insa-lyon.fr>
2022-03-14 16:48:24 +0100

    Première vue de la page

---
+++ pages/FT.vue
@@ -0,0 +1,53 @@
+<template>
+<v-app>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="white"
+    
+      scroll-target="#scrolling-techniques-4"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Ta fiche tâche</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-4"
+      class="overflow-y-auto"
+      max-height="667"
+    >
+      <v-card min-height="300" style="height:667px">
+        <v-card-text><br><br>BONSOIR !
+
+On a malheureusement pas une quantité infinie de matos!
+
+Mais heureusement nos chers amis du BDE sont là, on va leur emprunter une première vague de matos !
+
+RDV local pour prendre les clés du Camion (20m3) et direction la MDE !
+
+Va a l'accueil et demande à voir le responsable log (ANTONIN CANAL)
+
+Tu lui demandes la liste de matos suivante :
+
+1 Transpalette
+
+Max Table de réception extérieur
+
+Max Banc de réception extérieur
+
+10 Cagette verte pliable
+
+Ensuite tu amènes tout ça au chalet backline (range ça bien que ça gène pas !) si le chalet est pas encore là , tu déposes tout ça au local !
+
+Ensuite tu ramènes le camion au local ! et tu reposes les clefs !</v-card-text>
+      </v-card>
+      
+    </v-sheet>
+  </v-card>
+
+</v-app>
+</template>

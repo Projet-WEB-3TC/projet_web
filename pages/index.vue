e354004bc7a303107b4964e1c18700c3ff96192e
AlexDevauchelle <devauchelle.alex@gmail.com>
2022-03-24 11:20:16 +0100

    Notification History

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,78 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6">
+          <v-btn elevation="2" v-on:click="scheduleNotfication()"
+            >Send Notification</v-btn
           >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
+        </v-col>
+
+        <v-col md="10">
+          <v-data-table
+            style="max-height: 100%; overflow-y: auto"
+            :headers="headers"
+            :items="notificationsPending"
+            class="elevation-1"
+            dense
+            :items-per-page="20"
           >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
+          </v-data-table>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { LocalNotifications } from '@capacitor/local-notifications'
+
 export default {
   name: 'IndexPage',
+
+  data() {
+    return {
+      users: [],
+      notificationsPending: [],
+      headers: [
+        { text: 'Titre', value: 'title' },
+        { text: 'Corps', value: 'body' },
+        { text: 'Date', value: 'date' },
+      ],
+    }
+  },
+
+  methods: {
+    scheduleNotfication() {
+      if (LocalNotifications.checkPermissions() === 'denied') {
+        LocalNotifications.requestPermissions()
+      }
+      if (LocalNotifications.checkPermissions() !== 'denied') {
+        LocalNotifications.schedule({
+          notifications: [
+            {
+              title: 'New Notif',
+              body: "i'm the body",
+              id: 1,
+              schedule: { at: new Date(Date.now() + 1000 * 5) },
+              sound: null,
+              attachments: null,
+              actionTypeId: '',
+              extra: null,
+            },
+          ],
+        })
+      }
+      this.notificationsPending.push({
+        title: 'New Notif',
+        body: "i'm the body",
+        date:
+          new Date(Date.now() + 1000 * 5).toLocaleDateString('fr') +
+          ' ' +
+          new Date(Date.now() + 1000 * 5).toLocaleTimeString('fr'),
+      })
+    },
+  },
 }
 </script>

c99a8544507f7bf48afe8ddc8df32b2d2431226e
YasserIssam <yasser.issam@insa-lyon.fr>
2022-04-04 15:37:47 +0200

    Rajout d'un V-Dialog pour les paramètres de l'application.

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -15,12 +15,6 @@
         <span>Annuaire</span>
         <v-icon>mdi-account</v-icon>
       </v-btn>
-
-       <v-btn value="Notif" to="/notif">
-        <span>Notification</span>
-        <v-icon>mdi-bell</v-icon>
-      </v-btn>
-
     </v-bottom-navigation>
     <v-app-bar fixed app :style="isJauneActive ? jauneStyle : ''">
       <v-img
@@ -36,13 +30,99 @@
       ></v-img>
       <v-spacer />
       <v-app-bar-nav-icon>
-        <v-btn icon @click="toggleTheme">
-          <v-icon>mdi-theme-light-dark</v-icon>
-        </v-btn>
+        <v-dialog
+          v-model="dialog"
+          fullscreen
+          hide-overlay
+          transition="dialog-bottom-transition"
+        >
+          <template #activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-wrench</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-toolbar :style="isJauneActive ? jauneStyle : ''">
+              <v-btn
+                icon
+                @click="
+                  dialog = false
+                  toggleTheme
+                "
+              >
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>Paramètres</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn
+                  text
+                  @click="
+                    dialog = false
+                    toggleTheme
+                  "
+                >
+                  Save
+                </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+            <v-list three-line subheader>
+              <v-subheader>General</v-subheader>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-checkbox v-model="notifications"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Notifications</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Activer les notifications sur votre Overbookd
+                    personnel.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-checkbox v-model="sound"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Sound</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Activer les sons de l'application.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-btn icon @click="toggleTheme">
+                    <v-icon>mdi-theme-light-dark</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Dark Mode</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Activer le thème Dark ou Light de
+                    Overbookd.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-btn icon @click="logout()">
+                    <v-icon>mdi-logout</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Se Déconnecter</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Je souhaite de quitter la meilleure application de tous les
+                    temps.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-dialog>
       </v-app-bar-nav-icon>
-      <v-btn icon @click="logout()">
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container style="max-width: none">
@@ -69,6 +149,10 @@ export default {
       version,
       file: undefined,
       stepDetail: undefined,
+      dialog: false,
+      notifications: false,
+      sound: true,
+      widgets: false,
     }
   },
 
@@ -102,7 +186,7 @@ export default {
         this.isJauneActive = true
       }
       this.counter++
-      if(this.isJauneActive === true && this.counter > 20){
+      if (this.isJauneActive === true && this.counter > 20) {
         this.isJauneActive = false
         this.counter = 0
       }

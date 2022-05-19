f23b5517249e6fbcd418d2020595b5c72acd0dcc
YasserIssam <91474078+YasserIssam@users.noreply.github.com>
2022-05-19 18:42:10 +0200

    Implemented login page and settings for layout.

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app dark>
     <v-bottom-navigation fixed app :style="isJauneActive ? jauneStyle : ''">
-      <v-btn value="Accueil" to="/">
+      <v-btn value="Accueil" to="/acceuil">
         <span>Accueil</span>
-        <v-icon>mdi-apps</v-icon>
+        <v-icon>mdi-home</v-icon>
       </v-btn>
 
       <v-btn value="Calendar" to="/calendar">
@@ -12,12 +12,12 @@
       </v-btn>
 
       <v-btn value="Trombi" to="/trombinoscope">
-        <span>Trombinoscope</span>
-        <v-icon>mdi-image</v-icon>
+        <span>Annuaire</span>
+        <v-icon>mdi-account</v-icon>
       </v-btn>
 
       <v-btn value="Notif" to="/notif">
-        <span>Notification</span>
+        <span>Notifications</span>
         <v-icon>mdi-bell</v-icon>
       </v-btn>
     </v-bottom-navigation>
@@ -35,13 +35,99 @@
       ></v-img>
       <v-spacer />
       <v-app-bar-nav-icon>
-        <v-btn icon @click="toggleTheme">
-          <v-icon>mdi-theme-light-dark</v-icon>
-        </v-btn>
+        <v-dialog
+          v-model="dialog"
+          fullscreen
+          hide-overlay
+          transition="dialog-bottom-transition"
+        >
+          <template #activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-toolbar :style="isJauneActive ? jauneStyle : ''">
+              <v-btn
+                icon
+                @click="
+                  dialog = false
+                  toggleTheme
+                "
+              >
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>Paramètres</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn
+                  text
+                  @click="
+                    dialog = false
+                    toggleTheme
+                  "
+                >
+                  Save
+                </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+            <v-list three-line subheader>
+              <v-subheader>General</v-subheader>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-checkbox v-model="notifications"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Notifications</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Activer les notifications sur votre Overbookd
+                    personnel.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-checkbox v-model="sound"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Sound</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Activer les sons de l'application.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-btn icon @click="toggleTheme">
+                    <v-icon>mdi-theme-light-dark</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Dark Mode</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Activer le thème Dark ou Light de
+                    Overbookd.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-btn value="Deconnecter" to="/" icon @click="logout()">
+                    <v-icon>mdi-logout</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Se Déconnecter</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Je souhaite de quitter la meilleure application de tous les
+                    temps.</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-dialog>
       </v-app-bar-nav-icon>
-      <v-btn icon @click="logout()">
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container style="max-width: none">
@@ -52,62 +138,67 @@
 </template>
 
 <script>
-const { version } = require("../package.json");
+const { version } = require('../package.json')
 
 export default {
-  name: "DefaultLayout",
+  name: 'DefaultLayout',
   data() {
     return {
       drawer: false,
       isWhiteMode: true, // let this set to true
       counter: 0,
       isJauneActive: false,
-      jauneStyle: "background-color: #FFD13C; color: #003C71",
+      jauneStyle: 'background-color: #FFD13C; color: #003C71',
       isDialogOpen: false,
       zoomEnabled: false,
       version,
       file: undefined,
       stepDetail: undefined,
-    };
+      dialog: false,
+      notifications: false,
+      sound: true,
+      widgets: false,
+    }
   },
 
   computed: {
     isMobile() {
-      return this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs;
+      return this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs
     },
     logo() {
       if (this.isJauneActive) {
-        return "Ricard.png";
+        return 'Ricard.png'
       }
       return this.$vuetify.theme.dark
-        ? "overbookd_logo_blanc.png"
-        : "overbookd_logo_noir.png";
+        ? 'overbookd_logo_blanc.png'
+        : 'overbookd_logo_noir.png'
     },
   },
 
   mounted() {
-    this.$vuetify.theme.dark = localStorage.theme || false;
+    this.$vuetify.theme.dark = localStorage.theme || false
   },
 
   methods: {
     toggleTheme() {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-      localStorage.theme = this.$vuetify.theme.dark;
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.theme = this.$vuetify.theme.dark
     },
 
     clickOnTitle() {
-      this.counter++;
+      this.counter++
       if (this.counter > 10) {
-        this.isJauneActive = true;
+        this.isJauneActive = true
       }
-      this.counter++;
+      this.counter++
       if (this.isJauneActive === true && this.counter > 20) {
-        this.isJauneActive = false;
-        this.counter = 0;
+        this.isJauneActive = false
+        this.counter = 0
       }
     },
   },
-};
+}
 </script>
 
 <style></style>
+

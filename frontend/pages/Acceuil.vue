da3dd39fd84f6fe903df312f6924e9e3ea5a07dc
TristanDevin <tristan.devin@insa-lyon.fr>
2022-05-20 01:45:55 +0200

    add-v-card-accueil

--- frontend/pages/Acceuil.vue
+++ frontend/pages/Acceuil.vue
@@ -1,1 +1,25 @@
-<template><div></div></template>
+<template>
+  <v-col cols="12">
+    <v-card color="#385F73" dark class="justify-center">
+      <v-card-title class="text-h5">
+        Bienvenue sur Overbookd (username)
+      </v-card-title>
+
+      <v-card-subtitle
+        ><br />
+        est pour le moment une version bêta, mais bientôt vous pourrez retrouver
+        ici toutes les fonctionnalités d'Overbookd adaptées pour mobile !
+      </v-card-subtitle>
+
+      <v-card-actions>
+        <v-btn text> Télécharger votre planning </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>

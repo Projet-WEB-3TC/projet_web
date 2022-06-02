c2ad1cd497973344642a83f4fc577c13d6a0e607
AlexDevauchelle <devauchelle.alex@gmail.com>
2022-06-02 11:04:09 +0200

    Cleaning back of unsued files

--- node_modules/nuxt-typed-vuex/template/plugin.js
+++
@@ -1,9 +0,0 @@
-import { getAccessorFromStore } from 'typed-vuex'
-
-import { createStore } from '<%= options.store %>'
-
-const storeAccessor = getAccessorFromStore(createStore())
-
-export default async ({ store }, inject) => {
-  inject('accessor', storeAccessor(store))
-}
